f94f2f9d7747f28248be52b7d68e66116ae8b873
Dan Daskivich <daskivich.d@husky.neu.edu>
2018-04-16 17:36:47 -0400

    Implemented channel join functionality, including intial payload return.

--- lib/stormchat_web/channels/alerts_channel.ex
+++ lib/stormchat_web/channels/alerts_channel.ex
@@ -1,13 +1,30 @@
 defmodule StormchatWeb.AlertsChannel do
   use StormchatWeb, :channel
 
-  def join("alerts:" <> alert_id, payload, socket) do
-    alert = Stormchat.Alerts.get_alert(alert_id)
+  alias Stormchat.Locations
+  alias Stormchat.Posts
 
-    if alert == nil do
-      {:error, %{reason: "no such alert"}}
-    else
-      {:ok, %{"alert" => alert}, socket}
+  # authenticates socket tocken, then returns the following payload
+  # - alert: the alert for this channel
+  # - polygons: a list of the polygons affected by this channel's alert
+  # - post: a list of the latest posts for this channel's alert
+  # - users: a list of users with saved locations affected by this channel's alert
+  def join("alerts:" <> alert_id, payload, socket) do
+    case Phoenix.Token.verify(conn, "auth token", socket.assigns[:token], max_age: 86400) do
+      {:ok, user_id} ->
+        alert = Stormchat.Alerts.get_alert(alert_id)
+
+        if alert == nil do
+          {:error, %{reason: "no such alert"}}
+        else
+          polygons = Locations.get_affected_polygons(alert.id)
+          posts = Posts.get_latest_posts(alert.id)
+          users = Users.get_affected_users(alert.id)
+
+          {:ok, %{"alert" => alert, "polygons" => polygons, "posts" => posts, "users" => users}, socket}
+        end
+      _else ->
+      {:error, %{reason: "invalid authorization token"}}
     end
   end
 
